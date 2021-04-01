69a9f24716b6d9e0d9938a8c52f3b3e4c69e57f6
zhengge-lang <794303044@qq.com>
2021-04-01 16:26:58 +0800

    将svg引入icon.vue

---
+++ src/icon.vue
@@ -0,0 +1,20 @@
+<template>
+    
+         <svg class="g-icon" ><use :xlink:href="`#i${name}`"></use></svg>
+        
+</template>
+<script>
+export default {
+  props:['name']
+// 为了避免iconPosition传的是undefined，所以要设置默认值left
+   
+}
+</script>
+<style lang="scss">
+       
+        .g-icon{
+            width: 1em;
+            height: 1em;
+            }
+       
+</style>
