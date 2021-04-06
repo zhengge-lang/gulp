89ac5e5e6f6377bfbf7d6405840ab2b7ea0b885a
zhengge-lang <794303044@qq.com>
2021-04-06 16:44:52 +0800

    创建button-group

---
+++ src/button-group.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="g-button-group">
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss">
+    .g-button-group{
+        display:inline-flex ;
+        vertical-align: middle;
+        > .g-button{
+            border-radius:0 ;
+           
+        }
+    }
+</style>
